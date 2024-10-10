75314178d2d8e9fede2dc6466fa8a4a82db1a8eb
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-10-10 16:11:15 +0530

    refactor navbar componetn UI

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -21,7 +21,7 @@ export function ModeToggle() {
         <Button
           variant="outline"
           size="icon"
-          className="dark:border-2 dark:border-gray-400 border-2 border-black"
+          className="dark:border-2 dark:border-gray-400 border-2 border-black w-20"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-black" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,11 @@
 import React from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
-import { User } from "next-auth";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./ModeToggle";
 
 function Navbar() {
   const { data: session } = useSession();
-
-  const user: User = session?.user as User;
 
   return (
     <nav className="p-4 md:p-6 shadow-md dark:shadow-gray-600 bg-gray-900 text-white">
@@ -24,21 +21,8 @@ function Navbar() {
           </a>
         </div>
         <div className="flex gap-2">
-          {!session && (
-            <Link href="/sign-up" className="flex justify-end">
-              <Button
-                className="w-full md:w-auto dark:bg-black dark:text-slate-200 dark:border-2 dark:border-slate-500 dark:hover:text-slate-300 bg-slate-100 text-black"
-                variant={"outline"}
-              >
-                Sign up
-              </Button>
-            </Link>
-          )}
           {session ? (
             <>
-              <span className="mr-4 flex items-center">
-                Welcome, {user?.username || user?.email}
-              </span>
               <Button
                 className="w-full md:w-auto bg-slate-100 text-black hover:text-white hover:border-white hover:border-2"
                 variant={"dark"}
